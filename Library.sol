a63eee35ec8ab41b31dd97b4d8aef79bc6d128ca
Subashchandrabose <52975478+Subas33@users.noreply.github.com>
2021-01-09 22:38:48 +0530

    Commit 2

--- Library.sol
+++ Library.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.6.12;
 
 library myLibrary {
     
-    // enum role = {Customer, Hotel, DeliveryExecutive};
+    
      struct UserReg {
         string name;  
         string contactAddress;
@@ -14,34 +14,20 @@ library myLibrary {
     
     struct hotelMenuCard {
         string name;
-        int256 price;
+        uint256 price;
+    }
+    
+    struct payment{
+        address hoteladdress;
+        uint256 amount;
+    }
+    
+    
+    struct Auth{
+        bool[] autharray;
     }
     
 }
-    //  function CustomerRegistration(string memory CustomerName, string memory ContactAddress,int8 PhoneNumber) public {
-    //     // Registration Fee is no Needed as the Customer should buy some Tokens to Order something
-        
-    //     UserReg storage Reg = UserReg(Customername,ContactAddress,PhoneNumber,true, false, false);
-    //     UserRegistration[msg.sender] = Reg;
-    //     customerArray.push(msg.sender);
-        
-    // }
-    
-    //  function HotelRegistration(string memory HotelName, string memory ContactAddress, int8 PhoneNumber) public {
-    //     // Registration Fee is no Needed as the Customer should buy some Tokens to Order something
-        
-    //     UserReg storage Reg = UserReg(HotelName,ContactAddress,PhoneNumber, false, true, false);
-    //     UserRegistration[msg.sender] = Reg;
-    //     HotelArray.push(msg.sender);
-        
-    // }
-    
-    // function DeliveryExecutiveRegistration(string memory ExecutiveName, string memory ContactAddress, int8 PhoneNumber) public {
-    //     // Registration Fee is no Needed as the Customer should buy some Tokens to Order something
-        
-    //     UserReg storage Reg = UserReg(ExecutiveName,ContactAddress,PhoneNumber, false, false, true);
-    //     UserRegistration[msg.sender] = Reg;
-    //     ExecutiveArray.push(msg.sender);
-        
+
 
     
